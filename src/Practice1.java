3b32dc54cea32fbb3fad69698001547c1b390dbb
chirag thakkar <nilpathakkar1984@gmail.com>
2022-05-12 21:02:18 +0100

    gitpr1

---
+++ src/Practice1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+    import java.util.Arrays;
+import java.util.Collections;
+
+    public class Practice1 {
+
+
+        public static void main(String[] args) {
+            int[]array=new int[]{2,4,5,3,1};
+            System.out.println("Numbers are before shorting in Ascending order");
+            for (int i=0;i<array.length;i++)
+                System.out.println(array[i]);
+            Arrays.sort(array);
+            System.out.println("Numbers are in Ascending order");
+
+            for (int i = 0; i<array.length; i++){
+                System.out.println(array[i]);
+
+
+            }
+
+
+
+
+        }
+    }
+
